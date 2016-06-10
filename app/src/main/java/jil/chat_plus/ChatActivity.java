4ae7c4459714822773fd0b2ac305c4044c725342
ceokereke <ceokereke@yahoo.com>
2016-06-10 04:12:28 +0100

    Deleted content_main.xml name changes ImageView1 with send

--- app/src/main/java/jil/chat_plus/ChatActivity.java
+++ app/src/main/java/jil/chat_plus/ChatActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
